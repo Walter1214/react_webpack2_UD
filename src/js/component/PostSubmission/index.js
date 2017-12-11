72dc593426096ec3310352dc6aaba1f398bd6d1b
Walter <walter.huang@cloud-interactive.com>
2017-12-11 19:07:09 +0800

    post_submission 頁面新增

--- src/js/component/PostSubmission/index.js
+++ src/js/component/PostSubmission/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './style.scss';
 import postSubmissionLogo from '../../../asset/img/post_submission_logo.png';
+import postSubmissionPalette from '../../../asset/img/post_submission_palette.png';
+import postSubmissionControl from '../../../asset/img/post_submission_control.png';
+
 
 class PostSubmission extends React.Component {
 	constructor(props) {
@@ -14,8 +17,18 @@ class PostSubmission extends React.Component {
 				<a href="javascript:;" className="post__submission--buttom">
 					<img src={postSubmissionLogo}></img>
 				</a>
-				<div className="post__submission--overwrite">
-				
+				<div className="post__submission--popup">
+					<div className="post__submission--container">
+						<div className="post__submission--content">
+							<textarea placeholder="撰寫貼文..."></textarea>
+							<div className="post__submission_palette">
+								<img src={postSubmissionPalette}></img>
+							</div>
+							
+						</div>
+						<img src={postSubmissionControl}></img>
+						{/* <img src={postSubmissionContainer}></img> */}
+					</div>
 				</div>
 			</div>
 		);
