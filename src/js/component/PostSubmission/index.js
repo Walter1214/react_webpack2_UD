4677f528fd46d3e90673e8cb3b0d17c209c31376
Walter <walter.huang@cloud-interactive.com>
2017-12-12 15:01:42 +0800

    post_submission 功能新增

--- src/js/component/PostSubmission/index.js
+++ src/js/component/PostSubmission/index.js
@@ -3,35 +3,65 @@ import './style.scss';
 import postSubmissionLogo from '../../../asset/img/post_submission_logo.png';
 import postSubmissionPalette from '../../../asset/img/post_submission_palette.png';
 import postSubmissionControl from '../../../asset/img/post_submission_control.png';
-
+import postSubmissionVote from '../../../asset/img/post_submission_vote.png';
 
 class PostSubmission extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
+			IsOpenPopup:true,
+			openIndex: 0,
 		};
+		this.closePopup = this.closePopup.bind(this);
+		this.openPopup = this.openPopup.bind(this);
+		this.openVote = this.openVote.bind(this);
+		this.openDefault = this.openDefault.bind(this);
 	}
+
 	render() {
 		return (
 			<div className="post__submission">
-				<a href="javascript:;" className="post__submission--buttom">
+				<a href="javascript:;" className="post__submission--buttom" onClick={this.openPopup}>
 					<img src={postSubmissionLogo}></img>
 				</a>
-				<div className="post__submission--popup">
-					<div className="post__submission--container">
+				<div className={'post__submission--popup ' + (this.state.IsOpenPopup ? 'ON' : '')}>
+					<div className={'post__submission--container ' +(this.state.openIndex === 0 ? 'ON' : '')}>
 						<div className="post__submission--content">
+							<a href="javascript:;" className="post__submission--close-buttom" onClick={this.closePopup}>
+							</a>
 							<textarea placeholder="撰寫貼文..."></textarea>
-							<div className="post__submission_palette">
-								<img src={postSubmissionPalette}></img>
-							</div>
-							
+							<img src={postSubmissionPalette}></img>
 						</div>
-						<img src={postSubmissionControl}></img>
-						{/* <img src={postSubmissionContainer}></img> */}
+						<a href="javascript:;" className="post__submission_palette" onClick={this.openVote}>
+							<img src={postSubmissionControl}></img>
+						</a>
+					</div>
+					<div className={'post__submission--vote ' +(this.state.openIndex === 1 ? 'ON' : '')}>
+						<a href="javascript:;" onClick={this.openDefault}>
+							<img src={postSubmissionVote}></img>
+						</a>	
 					</div>
 				</div>
 			</div>
 		);
 	}
+
+
+	closePopup(){
+		this.setState({IsOpenPopup:false});
+		return false;
+	}
+	openPopup(){
+		this.setState({IsOpenPopup:true});
+	}
+
+	openVote(){
+		this.setState({openIndex:1});
+	}
+
+	openDefault(){
+		this.setState({openIndex:0});
+	}
+	
 }
 export default PostSubmission;
